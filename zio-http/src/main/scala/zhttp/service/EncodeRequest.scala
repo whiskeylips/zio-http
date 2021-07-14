eae111c8b654760bb540c8a5532c54d9c2d72317
shrutiverma97 <shruti@dream11.com>
2021-07-14 17:52:19 +0530

    refactor(getBodyAsString): removed unwanted changes

--- zio-http/src/main/scala/zhttp/service/EncodeRequest.scala
+++ zio-http/src/main/scala/zhttp/service/EncodeRequest.scala
@@ -1,11 +1,7 @@
 package zhttp.service
 
 import io.netty.buffer.{Unpooled => JUnpooled}
-import io.netty.handler.codec.http.{
-  HttpHeaderNames => JHttpHeaderNames,
-  HttpUtil => JHttpUtil,
-  HttpVersion => JHttpVersion,
-}
+import io.netty.handler.codec.http.{HttpHeaderNames => JHttpHeaderNames, HttpVersion => JHttpVersion}
 import zhttp.core.{JDefaultFullHttpRequest, JFullHttpRequest}
 import zhttp.http.{HTTP_CHARSET, Header, Request, Root}
 trait EncodeRequest {
@@ -14,27 +10,23 @@ trait EncodeRequest {
    * Converts Request to JFullHttpRequest
    */
   def encodeRequest(jVersion: JHttpVersion, req: Request): JFullHttpRequest = {
-    val method  = req.method.asJHttpMethod
-    val uri     = req.url.path match {
+    val method      = req.method.asJHttpMethod
+    val uri         = req.url.path match {
       case Root => "/"
       case _    => req.url.relative.asString
     }
-    val headers = Header.disassemble(req.headers)
-    val jReq    = new JDefaultFullHttpRequest(jVersion, method, uri)
-    jReq.headers().set(headers)
-
-    val charSet =
-      JHttpUtil.getCharset(jReq, HTTP_CHARSET)
-
-    val content         = req.getBodyAsString(charSet) match {
+    val content     = req.getBodyAsString() match {
       case Some(text) => JUnpooled.copiedBuffer(text, HTTP_CHARSET)
       case None       => JUnpooled.EMPTY_BUFFER
     }
-    val writerIndex     = content.writerIndex()
+    val headers     = Header.disassemble(req.headers)
+    val writerIndex = content.writerIndex()
     if (writerIndex != 0) {
       headers.set(JHttpHeaderNames.CONTENT_LENGTH, writerIndex.toString())
     }
-    val JReqWithContent = jReq.replace(content)
-    JReqWithContent
+    val jReq        = new JDefaultFullHttpRequest(jVersion, method, uri, content)
+    jReq.headers().set(headers)
+
+    jReq
   }
 }
